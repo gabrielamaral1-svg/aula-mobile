9bbd8f1e52f84088d478445ebc675ad79adf0829
GABRIEL BORBA DE AMARAL <gabriel.amaral1@acad.univates>
2026-08-07 21:46:47 -0300

    atualizacao

--- aula31072/App.js
+++ aula31072/App.js
@@ -1,14 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable, TouchableHighlight } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>verde</Text>
+          <Button
+  title="BOTAO"
+  color="#ff0000"
+  accessibilityLabel="Learn more about this purple button"></Button>
+      
+  <Pressable>
+  <Text>I'm pressable!</Text>
+</Pressable>
+
+<TouchableHighlight
+  activeOpacity={0.6}
+  underlayColor="#DDDDDD">
+    <Text>GOOGLE</Text> 
+</TouchableHighlight>;
+    
       <StatusBar style="auto" />
-    </View>
-  );
+     </View>
+ 
+
+
+
+);
+
+
+
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
